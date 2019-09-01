6f0227665934eb4557c183e1808cbac4502db4a8
bot-boi <bananajones532@gmail.com>
2019-08-31 21:00:43 -0400

    added scrolling functions, made get_links() work good

--- linkedin.py
+++ linkedin.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+import time
 
 from constants import user1
 
@@ -24,6 +25,22 @@ def login(user, pw): # logs into linkedin
 def wait_user(): # waits for user to press enter
     input("Press Enter to continue...")
 
+def scroll_once():
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+def scroll_to_bottom():
+    SCROLL_PAUSE_TIME = 1 #seconds
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+
+        last_height = new_height
+
 def open_sales_nav():
     driver.get("https://www.linkedin.com/sales/")
 
@@ -31,11 +48,19 @@ def open_sales_search():
     driver.get("https://www.linkedin.com/sales/search/people?viewAllFilters=true")
 
 def get_links(): # gets a list of links to sales nav profiles
-    elems = driver.find_elements_by_xpath("//*[@href]")
-    elems = [elem for elem in elems if "/sales/people" in elem.get_attribute("href")]
+    elems = []
+    for n in range(2):
+       elems += driver.find_elements_by_xpath("//*[@href]")
+       scroll_once()
+       time.sleep(2)
+
+    links = [elem.get_attribute("href") for elem in elems] # convert elems to string
+    links = [link for link in links if "/sales/people" in link] # filter links to profiles
+    links = set(links) # remove any duplicates
+    return links
 
 def init():
     global driver
     driver = webdriver.Chrome()
     login(user1.email, user1.pw)
-    open_sales_nav()
+    open_sales_search()
